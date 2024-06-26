cdb991e925f1e686b557aea8423441b6348c80cf
Lucas Nabesima <lucas.nabesima@gmail.com>
2024-06-26 06:32:02 -0300

    feat: implement withdrawal limits and refactor code
    In the banking operations, withdrawal limitations (quantity and amount)
    have been added for account security enhancements. The deposit,
    withdraw, and generate_statement functions have been improved and
    redundant code has been removed from the main script accordingly.

--- bank_operations.py
+++ bank_operations.py
@@ -3,16 +3,33 @@ import locale
 locale.setlocale(locale.LC_ALL, 'pt_BR.utf8')
 
 account_balance = 0.0
+MAX_WITHDRAW_LIMIT = 3
+MAX_WITHDRAW_AMOUNT = 500.0
+current_withdraw_amount = 0
 bank_statement = []
 
 
-def withdraw(amount: int):
+def withdraw(amount: float):
+    global current_withdraw_amount, account_balance
+
+    if current_withdraw_amount >= MAX_WITHDRAW_LIMIT:
+        raise ValueError("Erro ao realizar saque: Limite máximo de saques atingido.")
+
+    if amount > MAX_WITHDRAW_AMOUNT:
+        raise ValueError(f"Erro ao realizar saque: O valor máximo de saque é {locale.currency(MAX_WITHDRAW_AMOUNT)}.")
+
+    if amount > account_balance:
+        raise ValueError("Erro ao realizar saque: Saldo insuficiente.")
+
+    account_balance -= amount
+    current_withdraw_amount += 1
+    bank_statement.append(f"Saque: {locale.currency(amount)}")
+    print(f"Saque de {locale.currency(amount)} realizado com sucesso. Saldo atual: {locale.currency(account_balance)}")
     return
 
 
 def deposit(amount: float):
-    global account_balance
-    global bank_statement
+    global account_balance, bank_statement
 
     if amount < 0:
         raise ValueError(
@@ -22,9 +39,15 @@ def deposit(amount: float):
     bank_statement.append(f"Depósito: {locale.currency(amount)}")
     print(
         f"Depósito de {locale.currency(amount)} realizado com sucesso. Saldo atual: {locale.currency(account_balance)}")
-
     return
 
 
 def generate_statement():
+    if not bank_statement:
+        print(f"Não foram registradas transações nesta conta.")
+
+    for transaction in bank_statement:
+        print(transaction)
+
+    print(f"=== Saldo atual: {locale.currency(account_balance)} ===")
     return
--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-from bank_operations import deposit
-
-import locale
-
-locale.setlocale(locale.LC_ALL, 'pt_BR.utf8')
+from bank_operations import deposit, withdraw, generate_statement
 
 menu = """
 Bem vindo ao DIO Bank.
@@ -15,15 +11,6 @@ Selecione uma opção para continuar:
 [0] - Sair
 """
 
-account_balance = 0.0
-MAX_WITHDRAW_LIMIT = 3
-MAX_WITHDRAW_AMOUNT = 500.0
-current_withdraw_amount = 0
-bank_statement = []
-
-# TODO Separar as opções do menu em funções separadas
-# TODO A função `saque` deve receber parâmetros apenas por argumentos nomeados
-# TODO A função `extrato` deve receber parâmetros por argumentos posicionais E nomeados
 # TODO Criar duas novas funções: criar usuário (cliente do banco) e criar conta corrente (vincular com usuário)
 # TODO O usuário deve conter as seguintes informações: nome, data de nascimento, cpf e endereço.
 #  O endereço é uma string que contém os seguintes dados: logradouro, numero, bairro, cidade/estado
@@ -41,43 +28,23 @@ while True:
         amount = float(input("Digite o valor do deposito: "))
         try:
             deposit(amount)
-        except (ValueError, TypeError) as err:
+        except ValueError as err:
             print(err)
         print(menu)
 
     elif option == '2':
         print("Opção selecionada: Sacar")
-        if current_withdraw_amount >= MAX_WITHDRAW_LIMIT:
-            print("Você atingiu o limite máximo de saques diários. Tente novamente amanhã.")
-            print(menu)
-            continue
-
         amount = float(input("Digite o valor do saque: "))
-        if amount > MAX_WITHDRAW_AMOUNT:
-            print(f"O valor é maior do que o limite máximo de saque por operação.")
-            continue
+        try:
+            withdraw(amount=amount)
+        except ValueError as err:
+            print(err)
 
-        if account_balance > amount:
-            account_balance -= amount
-            current_withdraw_amount += 1
-            bank_statement.append(f"Saque: {locale.currency(amount)}")
-            print(
-                f"Saque de {locale.currency(amount)} realizado com sucesso. "
-                f"Saldo atual: {locale.currency(account_balance)}")
-            print(menu)
-        else:
-            print("Seu saldo é insuficiente para realizar esta operação.")
-            print(menu)
+        print(menu)
 
     elif option == '3':
         print("Opção selecionada: Extrato")
-        if not bank_statement:
-            print("Não foram registradas transações nesta conta.")
-            continue
-
-        for transaction in bank_statement:
-            print(transaction)
-        print(f"Saldo atual: {locale.currency(account_balance)}")
+        generate_statement()
         print(menu)
     elif option == '0':
         print("Opção selecionada: Sair")
